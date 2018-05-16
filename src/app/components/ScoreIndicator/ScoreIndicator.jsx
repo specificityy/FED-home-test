52bb2d3cd148e5f208a23432b1067c3018e4edd5
Wilson Jimenez <wilsonhjimenez@gmail.com>
2018-05-16 23:34:30 +0100

    Rough version of circular score indicator

--- src/app/components/ScoreIndicator/ScoreIndicator.jsx
+++ src/app/components/ScoreIndicator/ScoreIndicator.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './scoreIndicator.scss';
-import bemHelper from '../../utils/bem';
+import "./scoreIndicator.scss";
+import bemHelper from "../../utils/bem";
 
 class ScoreIndicator extends React.Component {
   render() {
     const cn = bemHelper({ block: this.props.block });
-    const { score, maxScoreValue, equifaxScoreBandDescription } = this.props.creditReport || {};
+    const { score, maxScoreValue, equifaxScoreBandDescription } =
+      this.props.creditReport || {};
     return (
-      <div className={cn('score-indicator')}>
+      <div className={cn("score-indicator")}>
         <div>Your credit score is</div>
         <div>{score}</div>
-        <div>out of <strong>{maxScoreValue}</strong></div>
+        <div>
+          out of <strong>{maxScoreValue}</strong>
+        </div>
         <div>{equifaxScoreBandDescription}</div>
       </div>
     );
@@ -20,7 +23,7 @@ class ScoreIndicator extends React.Component {
 }
 
 ScoreIndicator.propTypes = {
-  creditReport: PropTypes.object.isRequired,
+  creditReport: PropTypes.object.isRequired
 };
 
 export default ScoreIndicator;
